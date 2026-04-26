d191d4f3ae309a4bbdb838933a4dc3616947c32a
queiroziagoo <iagotqueiroz@gmail.com>
2026-04-26 13:39:45 -0300

    Função de mostrar os agendamentos na tela de acordo com o horário.

---
+++ src/js/show.js
@@ -0,0 +1,55 @@
+import dayjs from "dayjs";
+
+// Seleciona as sessões
+
+const periodMorning = document.getElementById("morning-list");
+const periodAfternoon = document.getElementById("afternoon-list");
+const periodEvening= document.getElementById("eveneing-list");
+
+export function scheduleShow({dailySchedules}) {
+    try {
+        // Limpa as listas
+        periodMorning.innerHTML = ""
+        periodAfternoon.innerHTML = ""
+        periodEvening.innerHTML = ""
+
+        // Renderiza os agendamentos por período
+        dailySchedules.forEach((schedule) => {
+            const item = document.createElement("li")
+            item.innerHTML = `
+                <article class="schedule-item">
+                    <div class="flex">
+                        <time datetime="" class="schedule-item_date">${schedule.scheduleTime}</time>
+                        <p class="schedule-item_name">
+                            <strong>${schedule.petName}</strong>
+                            / ${schedule.tutorName}
+                        </p>
+                    </div>
+
+                    <div>
+                        <p class="schedule-item_service">${schedule.serviceDescription}</p>
+                    </div>
+
+                    <div class="btn-box_remove  flex">
+                        <button type="button" class="btn-remove">Remover agendamento</button>
+                    </div>
+                </article>
+            `
+            
+            const hour = dayjs(`${schedule.scheduleDate} ${schedule.scheduleTime}`).hour()
+
+            if (hour >= 9 && hour <= 12) {
+                periodMorning.appendChild(item)
+            } else if (hour >= 13 && hour <= 18) {
+                periodAfternoon.appendChild(item)
+            } else if (hour >= 19 && hour <= 21) {
+                periodEvening.appendChild(item)
+            }
+        });
+
+        
+    } catch (error) {
+        console.log(error)
+        alert("Não foi possível exibir os agendamentos")
+    }
+}
